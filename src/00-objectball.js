3e2ac8cb647c45474185e2d767ffd7c33fee806f
Nijhan <najmashaaban6@gmail.com>
2025-06-15 00:46:23 +0300

    objects

---
+++ src/00-objectball.js
@@ -0,0 +1,31 @@
+function gameObject () {
+    return {
+        home: {
+            teamName: "Brooklyn Nets",
+            colors: ["Black", "White"],
+            players: {
+                AlanAnderson: {
+                    number:0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 1
+                },
+                ReggieEvans: {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamDunks: 15,Z
+
+                }
+            }
+        }
+    }
+}
